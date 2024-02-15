31cbe78290b810a9fdacf8847a3e9128c6ec8e7d
AndresGR00 <angare97@gmail.com>
2024-02-15 16:35:42 +0100

    Refactorize routers in index.js

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ require("dotenv").config({ path: "./src/utils/.env" });
 const express = require("express");
 const { connectDB } = require("./src/config/db");
 
-//Import routers
-const characterRouter = require("./src/api/routes/1-characters.router");
-const creatureRouter = require("./src/api/routes/2-creatures.router");
-const droidRouter = require("./src/api/routes/3-droids.router");
-const locationRouter = require("./src/api/routes/4-locations.router");
-const organizationRouter = require("./src/api/routes/5-organizations.router");
-const specieRouter = require("./src/api/routes/6-species.router");
-const vehicleRouter = require("./src/api/routes/7-vehicles.router");
-const weaponRouter = require("./src/api/routes/8-weapons.router");
-const moreRouter = require("./src/api/routes/9-more.router");
-
 const app = express();
 app.use(express.json());
 app.disable("x-powered-by");
@@ -21,17 +10,24 @@ const PORT = 3000;
 
 connectDB();
 
-//Routes
-app.use("/api/V1/characters", characterRouter);
-app.use("/api/V1/creatures", creatureRouter);
-app.use("/api/V1/droids", droidRouter);
-app.use("/api/V1/locations", locationRouter);
-app.use("/api/V1/organizations", organizationRouter);
-app.use("/api/V1/species", specieRouter);
-app.use("/api/V1/vehicles", vehicleRouter);
-app.use("/api/V1/weapons", weaponRouter);
-app.use("/api/V1/more", moreRouter);
+const apiRouter = express.Router();
+const routers = [
+  { path: "characters", router: require("./src/api/routes/1-characters.router") },
+  { path: "creatures", router: require("./src/api/routes/2-creatures.router") },
+  { path: "droids", router: require("./src/api/routes/3-droids.router") },
+  { path: "locations", router: require("./src/api/routes/4-locations.router") },
+  { path: "organizations", router: require("./src/api/routes/5-organizations.router") },
+  { path: "species", router: require("./src/api/routes/6-species.router") },
+  { path: "vehicles", router: require("./src/api/routes/7-vehicles.router") },
+  { path: "weapons", router: require("./src/api/routes/8-weapons.router") },
+  { path: "more", router: require("./src/api/routes/9-more.router") },
+];
 
+routers.forEach(({ path, router }) => {
+  apiRouter.use(`/api/V1/${path}`, router);
+});
+
+app.use(apiRouter);
 
 app.use("*", (req, res, next) => {
   return res.status(404).json("Not found");
